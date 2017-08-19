62b97a684d85410dbfb7a9239ee7bfbdfdf7e9a5
AlisonT <alison.tong@mail.utoronto.ca>
2017-08-18 22:27:57 -0400

    Organized into functions and added a global variables file

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import pygame
 import settings
+import global_vars
 import os
 from cat import Cat
 import time
@@ -10,35 +11,39 @@ display = pygame.display.set_mode((settings.screen_width, settings.screen_height
 pygame.display.set_caption('Catastrophe')
 clock = pygame.time.Clock()
 
-game_exit = False
-default_background_col = (255, 255, 255)
-black = (0, 0, 0)
+player = Cat(100, 100, 'black')
+player_img = pygame.image.load(os.path.join(player.img_src))
+player_img = pygame.transform.scale(player_img, (100,100))
+player_img.convert()
 
-bean = Cat(100, 100, 'black')
-bean_img = pygame.image.load(os.path.join(bean.img_src))
-bean_img = pygame.transform.scale(bean_img, (100,100))
-bean_img.convert()
+def update():
+    player.update()
 
-while not game_exit:
-    display.fill(default_background_col)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_exit = True
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_LEFT]:
-            bean.dx = -10
-        elif pressed[pygame.K_RIGHT]:
-            bean.dx = 10
-        else:
-            bean.dx = 0
-        if pressed[pygame.K_UP] and bean.y == settings.screen_height - bean.height:
-            bean.dy = -20
-        
-    bean.update()
-    display.blit(bean_img, (bean.x, bean.y))
+def game_loop():
+    while not global_vars.game_exit:
+        display.fill(global_vars.default_background_col)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                global_vars.game_exit = True
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_LEFT]:
+                player.dx = -10
+            elif pressed[pygame.K_RIGHT]:
+                player.dx = 10
+            else:
+                player.dx = 0
+            if pressed[pygame.K_UP] and player.y == settings.screen_height - player.height:
+                player.dy = -20
+        update()
+        display.blit(player_img, (player.x, player.y))
 
-    pygame.display.update()
-    clock.tick(60)
+        pygame.display.update()
+        clock.tick(60)
 
-pygame.quit()
-quit()
+
+if __name__ == '__main__':
+    game_loop()
+
+    pygame.quit()
+    quit()
+    
---
+++ global_vars.py
@@ -0,0 +1,3 @@
+game_exit = False
+default_background_col = (255, 255, 255)
+black = (0, 0, 0)
