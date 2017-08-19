87abf45fcd3b36dcaab1893843c891e6851f253c
AlisonT <alison.tong@mail.utoronto.ca>
2017-08-18 20:38:26 -0400

    Working cat class with gravity implemented

--- __init__.py
+++ __init__.py
@@ -1,18 +1,44 @@
 import pygame
+import settings
+import os
+from cat import Cat
+import time
 
 pygame.init()
 
-display = pygame.display.set_mode((800, 600))
+display = pygame.display.set_mode((settings.screen_width, settings.screen_height))
 pygame.display.set_caption('Catastrophe')
+clock = pygame.time.Clock()
 
 game_exit = False
+default_background_col = (255, 255, 255)
+black = (0, 0, 0)
 
-pygame.display.update()
+bean = Cat(100, 100, 'black')
+bean_img = pygame.image.load(os.path.join(bean.img_src))
+bean_img = pygame.transform.scale(bean_img, (100,100))
+bean_img.convert()
 
 while not game_exit:
+    display.fill(default_background_col)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_exit = True
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]:
+            bean.dx = -5
+        elif pressed[pygame.K_RIGHT]:
+            bean.dx = 5
+        else:
+            bean.dx = 0
+        if pressed[pygame.K_UP]:
+            bean.dy = -15
+        
+    bean.update()
+    display.blit(bean_img, (bean.x, bean.y))
+
+    pygame.display.update()
+    clock.tick(60)
 
 pygame.quit()
 quit()
---
+++ cat.py
@@ -0,0 +1,29 @@
+import settings
+
+class Cat:
+
+    def __init__(self, x, y, col):
+        self.x, self.y, self.dx, self.dy = x, y, 0, 0
+        self.width, self.height = 100, 100
+        self.img_src = "textures/cats/" + col + "_cat.png"
+    
+    def update(self):
+        if self.dx > 0:
+            if self.x < settings.screen_width - self.width:
+                self.x += self.dx
+            else:
+                self.x = settings.screen_width - self.width
+        elif self.dx < 0:
+            if self.x > 0:
+                self.x += self.dx
+            else:
+                self.x = 0
+        if self.dy > 0:
+            if self.y < settings.screen_height - self.height:
+                self.y += self.dy
+            else:
+                self.y = settings.screen_height - self.height
+        elif self.dy < 0:
+            if self.y > 0:
+                self.y += self.dy
+        self.dy += 1
