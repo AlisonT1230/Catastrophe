bd7696496252a2286cf587c8f939a1acadf7ffcd
AlisonT <alison.tong@mail.utoronto.ca>
2017-08-20 22:23:16 -0400

    Added blankets

--- blanket.py
+++ blanket.py
@@ -13,3 +13,16 @@ class Blanket(Tangible):
         self.green_img = pygame.image.load(os.path.join("textures/blocks/green_blanket.bmp"))
         self.green_img = pygame.transform.scale(self.green_img, (self.width, self.height))
         self.green_img.convert()
+
+        self.blue_img = pygame.image.load(os.path.join("textures/blocks/blue_blanket.bmp"))
+        self.blue_img = pygame.transform.scale(self.blue_img, (self.width, self.height))
+        self.blue_img.convert()
+
+        self.purple_img = pygame.image.load(os.path.join("textures/blocks/purple_blanket.bmp"))
+        self.purple_img = pygame.transform.scale(self.purple_img, (self.width, self.height))
+        self.purple_img.convert()
+
+        self.red_img = pygame.image.load(os.path.join("textures/blocks/red_blanket.bmp"))
+        self.red_img = pygame.transform.scale(self.red_img, (self.width, self.height))
+        self.red_img.convert()
+        
