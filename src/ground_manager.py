60d421539a719fe2125e335953aa439ffb409d79
AlisonT <alison.tong@mail.utoronto.ca>
2017-08-25 18:29:21 -0400

    Split draw function, added more compact way to draw groups of blocks, added checks for block position before drawing (don't draw outside of screen)

--- src/ground_manager.py
+++ src/ground_manager.py
@@ -1,4 +1,4 @@
-
+from ground import Ground
 
 class GroundManager:
 
@@ -6,3 +6,11 @@ class GroundManager:
         self.blankets = []
         self.ground_blocks = []
         
+    def build_group(self, x1, y1, x2, y2, type):
+        """
+        Builds a collection of type ground blocks according to x and y coordinates.
+        """
+        for x in range (x1, x2 + 100, 100):
+            for y in range (y1, y2 + 100, 100):
+                self.ground_blocks.append(Ground(x, y, type))
+                
